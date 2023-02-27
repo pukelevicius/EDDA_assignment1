f108ff27a506cb04f048ca147a413b49f46e69c8
Silver Lee-A-Fong <silverlee2000@gmail.com>
2023-02-27 11:32:09 +0100

    Changes to 3

--- exercise3.R
+++ exercise3.R
@@ -7,7 +7,7 @@ data <- read.csv("Data/diet.txt",header = TRUE, sep = "")
 # a)
 # Test the assumptions
 # Check the normality of the data
-hist(data$weight6weeks - data$preweight, breaks=10)
+hist(data$preweight - data$weight6weeks, breaks=10)
 
 # Test normality of the data
 aovdata = lm(preweight~weight6weeks, data=data)
@@ -25,52 +25,39 @@ t.test(data$preweight, data$weight6weeks, paired=TRUE)
 
 # - If normality assumption is not met, Kruskal-wallis can be used
 #data$weight6weeks = as.factor(data$weight6weeks)
-model_b <- aov(preweight - weight6weeks ~ diet, data=data)
-summary(model_b)
+model_b <- lm(preweight - weight6weeks ~ diet, data=data)
+anova(model_b)
+# summary(model_b)
 
 # Do all three types diets lead to weight loss?
 # p-value is < 0.05 which indicates there is a significant mean difference
 # From this we could say that on average there is a significant weight loss
 
 # Which diet was the best for losing weight? 
-diet_1 = filter(data, diet==1) 
-diet_2 = filter(data, diet==2) 
-diet_3 = filter(data, diet==3)
-
-mean(diet_1$preweight) - mean(diet_1$weight6weeks)
-mean(diet_2$preweight) - mean(diet_2$weight6weeks)
-mean(diet_3$preweight) - mean(diet_3$weight6weeks)
-
 # Create a boxplot of weight6weeks by diet to compare the weight loss between the diets
 boxplot((preweight-weight6weeks) ~ diet, data = data, xlab = "Diet", ylab = "Weight loss", main = "Boxplot of weightloss by Diet")
 # The best diet is diet 3 because on average the weight has decreased the most
 
-# Can the Kruskal-Wallis test be applied for this situation?
-# Kruskal-Wallis can test whether medians are different for multiple groups.
-# The outcome would describe whether the effects of the three diets are
-# the same or not on the weight lost.
-# Kruskal can be applied but won't provide significant data to tell which
-# diet was best for losing weight.
-
-# c)
-# Fit a two-way ANOVA model
-lostweights = as.factor(data$preweight - data$weight6weeks)
+# c) Fit a two-way ANOVA model
 data$gender = as.factor(data$gender)
+data$diet =  as.factor(data$diet)
 
-model_c <- aov(diet ~ lostweights * gender, data = data)
-summary(model_c)
+model_c <- lm(preweight - weight6weeks ~ diet * gender, data = data)
+anova(model_c)
 
-# p-value of lost weights:gender is > 0.05 which concludes that lost weight and gender have interaction
-# given the diet. It shows that diet has an effect on lost weight and gender.
+# Interaction of gender and diet gives a p-value < 0.05 which 
+# shows that diet and gender have an effect on the lost weight.
 
+# e) two-way anova is preferred
+diet_1 = filter(data, diet==1) 
+diet_2 = filter(data, diet==2) 
+diet_3 = filter(data, diet==3)
 
-# e)
+avg_loss_diet_1 = mean(diet_1$preweight) - mean(diet_1$weight6weeks)
+avg_loss_diet_2 = mean(diet_2$preweight) - mean(diet_2$weight6weeks)
+avg_loss_diet_3 = mean(diet_3$preweight) - mean(diet_3$weight6weeks)
 
+diet_1_predicted = mean(diet_1$preweight) - avg_loss_diet_1; diet_1_predicted
+diet_2_predicted = mean(diet_2$preweight) - avg_loss_diet_2; diet_2_predicted
+diet_3_predicted = mean(diet_3$preweight) - avg_loss_diet_3; diet_3_predicted
 
-
-# For the "A" diet, the predicted lost weight would be:
-# Mean lost weight for diet A + (Effect of gender on lost weight for diet A)
-# For the "B" diet, the predicted lost weight would be:
-# Mean lost weight for diet B + (Effect of gender on lost weight for diet B)
-# For the "C" diet, the predicted lost weight would be:
-# Mean lost weight for diet C + (Effect of gender on lost weight for diet C)
